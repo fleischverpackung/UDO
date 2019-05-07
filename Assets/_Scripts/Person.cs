3d974b4f40fd9052f128b202642ab134ab21a929
fleischverpackung <d_o_z_e_r@gmx.net>
2019-05-08 00:55:03 +0200

    mit audio aber geht noch net

--- Assets/_Scripts/Person.cs
+++ Assets/_Scripts/Person.cs
@@ -6,17 +6,28 @@ public class Person : MonoBehaviour
 {
 
     private Animator ani;
+    private AudioClip[] yeahs;
+    private AudioSource speaker;
+    //private int yeahCount;
 
 
     void Start()
     {
+
        ani = GetComponent<Animator>();
        ani.SetInteger("level", Random.Range(0, 7));
+       speaker = GetComponent<AudioSource>();
+       yeahs = Resources.LoadAll<AudioClip>("Assets/_Audio/yeah/");
+       // Debug.Log(yeahs.Length().ToString());
+       //yeahCount = yeahs.Length();
+
     }
 
     void Update()
     {
         new WaitForSeconds(Random.Range(3, 10));
         ani.SetInteger("level", Random.Range(0, 8));
+        //speaker.PlayOneShot(yeahs[1]);
+        
     }
 }
